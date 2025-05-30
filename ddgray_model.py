f79736934d9fa87dabb90a546a42f8e6c9a1ad1f
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2025-05-30 16:23:48 +0800

    Update ddgray_model.py

--- ddgray_model.py
+++ ddgray_model.py
@@ -1,26 +1,27 @@
 import torch
 import torch.nn as nn
 
-from basicsr.archs.ddcolor_arch_utils.unet import Hook, CustomPixelShuffle_ICNR, UnetBlockWide, NormType, custom_conv_layer
+from basicsr.archs.ddcolor_arch_utils.unet import Hook, CustomPixelShuffle_ICNR, UnetBlockWide, NormType, \
+    custom_conv_layer
 from basicsr.archs.ddcolor_arch_utils.convnext import ConvNeXt
 from basicsr.archs.ddcolor_arch_utils.transformer_utils import SelfAttentionLayer, CrossAttentionLayer, FFNLayer, MLP
 from basicsr.archs.ddcolor_arch_utils.position_encoding import PositionEmbeddingSine
 
 
-class DDGray(nn.Module):
+class GreyModel(nn.Module):
     def __init__(
-        self,
-        encoder_name='convnext-l',
-        decoder_name='MultiScaleGrayDecoder',
-        num_input_channels=1,
-        input_size=(256, 256),
-        nf=512,
-        num_output_channels=1,
-        last_norm='Weight',
-        do_normalize=False,
-        num_queries=256,
-        num_scales=3,
-        dec_layers=9,
+            self,
+            encoder_name='convnext-l',
+            decoder_name='MultiScaleColorDecoder',
+            num_input_channels=2,  # 输入2通道(XDoG + Gray)
+            input_size=(256, 256),
+            nf=512,
+            num_output_channels=1,  # 输出灰度图1通道
+            last_norm='Weight',
+            do_normalize=False,
+            num_queries=256,
+            num_scales=3,
+            dec_layers=9,
     ):
         super().__init__()
 
@@ -40,10 +41,13 @@ class DDGray(nn.Module):
         )
 
         self.refine_net = nn.Sequential(
-            custom_conv_layer(num_queries + num_input_channels, num_output_channels, ks=1, use_activ=False, norm_type=NormType.Spectral)
+            custom_conv_layer(num_queries + num_input_channels, num_output_channels, ks=1, use_activ=False,
+                              norm_type=NormType.Spectral)
         )
 
         self.do_normalize = do_normalize
+        # 灰度不做ImageNet标准化
+        # 但如果需要，可以定义自己的mean/std
         self.register_buffer('mean', torch.Tensor([0.5]).view(1, 1, 1, 1))
         self.register_buffer('std', torch.Tensor([0.5]).view(1, 1, 1, 1))
 
@@ -54,8 +58,9 @@ class DDGray(nn.Module):
         return img * self.std + self.mean
 
     def forward(self, x):
-        if x.shape[1] == 1:
-            x = self.normalize(x)
+        if x.shape[1] == 2:
+            if self.do_normalize:
+                x = self.normalize(x)
 
         self.encoder(x)
         out_feat = self.decoder()
@@ -71,13 +76,15 @@ class ImageEncoder(nn.Module):
     def __init__(self, encoder_name, hook_names):
         super().__init__()
 
+        assert encoder_name == 'convnext-t' or encoder_name == 'convnext-l'
         if encoder_name == 'convnext-t':
-            self.arch = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], in_chans=1)
+            self.arch = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768])
         elif encoder_name == 'convnext-l':
-            self.arch = ConvNeXt(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536], in_chans=1)
+            self.arch = ConvNeXt(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536])
         else:
             raise NotImplementedError
 
+        self.encoder_name = encoder_name
         self.hook_names = hook_names
         self.hooks = self.setup_hooks()
 
@@ -91,15 +98,15 @@ class ImageEncoder(nn.Module):
 
 class DuelDecoder(nn.Module):
     def __init__(
-        self,
-        hooks,
-        nf=512,
-        blur=True,
-        last_norm='Weight',
-        num_queries=256,
-        num_scales=3,
-        dec_layers=9,
-        decoder_name='MultiScaleGrayDecoder',
+            self,
+            hooks,
+            nf=512,
+            blur=True,
+            last_norm='Weight',
+            num_queries=256,
+            num_scales=3,
+            dec_layers=9,
+            decoder_name='MultiScaleColorDecoder',
     ):
         super().__init__()
         self.hooks = hooks
@@ -110,9 +117,11 @@ class DuelDecoder(nn.Module):
 
         self.layers = self.make_layers()
         embed_dim = nf // 2
-        self.last_shuf = CustomPixelShuffle_ICNR(embed_dim, embed_dim, blur=self.blur, norm_type=self.last_norm, scale=4)
+        self.last_shuf = CustomPixelShuffle_ICNR(embed_dim, embed_dim, blur=self.blur, norm_type=self.last_norm,
+                                                 scale=4)
 
-        self.gray_decoder = MultiScaleGrayDecoder(
+        assert decoder_name == 'MultiScaleColorDecoder'
+        self.color_decoder = MultiScaleColorDecoder(
             in_channels=[512, 512, 256],
             num_queries=num_queries,
             num_scales=num_scales,
@@ -130,7 +139,8 @@ class DuelDecoder(nn.Module):
             if layer_index == len(setup_hooks) - 1:
                 out_c = out_c // 2
             decoder_layers.append(
-                UnetBlockWide(in_c, feature_c, out_c, hook, blur=self.blur, self_attention=False, norm_type=NormType.Spectral))
+                UnetBlockWide(
+                    in_c, feature_c, out_c, hook, blur=self.blur, self_attention=False, norm_type=NormType.Spectral))
             in_c = out_c
 
         return nn.Sequential(*decoder_layers)
@@ -142,22 +152,22 @@ class DuelDecoder(nn.Module):
         out2 = self.layers[2](out1)
         out3 = self.last_shuf(out2)
 
-        return self.gray_decoder([out0, out1, out2], out3)
+        return self.color_decoder([out0, out1, out2], out3)
 
 
-class MultiScaleGrayDecoder(nn.Module):
+class MultiScaleColorDecoder(nn.Module):
     def __init__(
-        self,
-        in_channels,
-        hidden_dim=256,
-        num_queries=100,
-        nheads=8,
-        dim_feedforward=2048,
-        dec_layers=9,
-        pre_norm=False,
-        gray_embed_dim=1,
-        enforce_input_project=True,
-        num_scales=3,
+            self,
+            in_channels,
+            hidden_dim=256,
+            num_queries=100,
+            nheads=8,
+            dim_feedforward=2048,
+            dec_layers=9,
+            pre_norm=False,
+            color_embed_dim=256,
+            enforce_input_project=True,
+            num_scales=3,
     ):
         super().__init__()
 
@@ -183,21 +193,39 @@ class MultiScaleGrayDecoder(nn.Module):
 
         for _ in range(dec_layers):
             self.transformer_self_attention_layers.append(
-                SelfAttentionLayer(d_model=hidden_dim, nhead=nheads, dropout=0.0, normalize_before=pre_norm)
+                SelfAttentionLayer(
+                    d_model=hidden_dim,
+                    nhead=nheads,
+                    dropout=0.0,
+                    normalize_before=pre_norm,
+                )
             )
             self.transformer_cross_attention_layers.append(
-                CrossAttentionLayer(d_model=hidden_dim, nhead=nheads, dropout=0.0, normalize_before=pre_norm)
+                CrossAttentionLayer(
+                    d_model=hidden_dim,
+                    nhead=nheads,
+                    dropout=0.0,
+                    normalize_before=pre_norm,
+                )
             )
             self.transformer_ffn_layers.append(
-                FFNLayer(d_model=hidden_dim, dim_feedforward=dim_feedforward, dropout=0.0, normalize_before=pre_norm)
+                FFNLayer(
+                    d_model=hidden_dim,
+                    dim_feedforward=dim_feedforward,
+                    dropout=0.0,
+                    normalize_before=pre_norm,
+                )
             )
 
         self.decoder_norm = nn.LayerNorm(hidden_dim)
-        self.gray_embed = MLP(hidden_dim, hidden_dim, gray_embed_dim, 3)
+
+        self.color_embed = MLP(hidden_dim, hidden_dim, color_embed_dim, 3)
 
     def forward(self, x, img_features):
         assert len(x) == self.num_feature_levels
+
         src, pos = self._get_src_and_pos(x)
+
         bs = src[0].shape[1]
 
         query_embed = self.query_embed.weight.unsqueeze(1).repeat(1, bs, 1)
@@ -206,17 +234,23 @@ class MultiScaleGrayDecoder(nn.Module):
         for i in range(self.num_layers):
             level_index = i % self.num_feature_levels
             output = self.transformer_cross_attention_layers[i](
-                output, src[level_index], memory_mask=None, memory_key_padding_mask=None,
+                output, src[level_index],
+                memory_mask=None,
+                memory_key_padding_mask=None,
                 pos=pos[level_index], query_pos=query_embed
             )
             output = self.transformer_self_attention_layers[i](
-                output, tgt_mask=None, tgt_key_padding_mask=None, query_pos=query_embed
+                output, tgt_mask=None,
+                tgt_key_padding_mask=None,
+                query_pos=query_embed
             )
             output = self.transformer_ffn_layers[i](output)
 
         decoder_output = self.decoder_norm(output).transpose(0, 1)
-        gray_embed = self.gray_embed(decoder_output)
-        out = torch.einsum("bqc,bchw->bqhw", gray_embed, img_features)
+        color_embed = self.color_embed(decoder_output)
+
+        out = torch.einsum("bqc,bchw->bqhw", color_embed, img_features)
+
         return out
 
     def _make_input_proj(self, in_ch, hidden_dim, enforce):
@@ -232,5 +266,6 @@ class MultiScaleGrayDecoder(nn.Module):
         src, pos = [], []
         for i, feature in enumerate(x):
             pos.append(self.pe_layer(feature).flatten(2).permute(2, 0, 1))
-            src.append((self.input_proj[i](feature).flatten(2) + self.level_embed.weight[i][None, :, None]).permute(2, 0, 1))
+            src.append(
+                (self.input_proj[i](feature).flatten(2) + self.level_embed.weight[i][None, :, None]).permute(2, 0, 1))
         return src, pos
